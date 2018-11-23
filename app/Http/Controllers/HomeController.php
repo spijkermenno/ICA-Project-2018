cd3da518f5366b38b572e6603b2d3e14e0fa933a
Menno Spijker <spijkermenno@gmail.com>
2018-11-23 13:42:28 +0100

    splited the navbar from the app.blade.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home', ['bla' => 1]);
+        return view('home', ['test' => 1]);
     }
 }
