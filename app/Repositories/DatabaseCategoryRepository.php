ca4e2250cec0dd8288e96ca026becd800019b5fc
joostlawerman <joostlawerman@users.noreply.github.com>
2018-11-26 15:57:55 +0100

    Use dependency injection instead of facade

--- app/Repositories/DatabaseCategoryRepository.php
+++ app/Repositories/DatabaseCategoryRepository.php
@@ -2,19 +2,18 @@
 
 namespace App\Repositories;
 
-use Illuminate\Support\Facades\DB;
 use App\Repositories\Contracts\CategoryRepositoryInterface;
 
 class DatabaseCategoryRepository extends DatabaseRepository implements CategoryRepositoryInterface
 {
     public function getAll()
     {
-        return DB::select('SELECT * FROM categories');
+        return $this->conn->select('SELECT * FROM categories');
     }
 
     public function getAllByParentId(int $id)
     {
-        return DB::select(
+        return $this->conn->select(
           'SELECT * FROM categories WHERE parent_id = ?',
           [$id]
       );
@@ -22,7 +21,7 @@ class DatabaseCategoryRepository extends DatabaseRepository implements CategoryR
 
     public function getById(int $id)
     {
-        return DB::select(
+        return $this->conn->select(
           'SELECT * FROM categories WHERE id = ?',
           [$id]
       );
