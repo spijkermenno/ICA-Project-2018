57fd3b1fe4e917fd9e380c920f92e84e9519bd98
Menno Spijker <spijkermenno@gmail.com>
2018-11-27 12:38:45 +0100

    fixed bootstrap js error and added some changes.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                        {{$test}}
-                </div>
-            </div>
-        </div>
+    <div class="container">
+        @include("breadcrumbs")
     </div>
-</div>
 @endsection
