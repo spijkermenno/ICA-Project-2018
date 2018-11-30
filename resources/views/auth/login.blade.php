84c13dc600bc7d74126ce58331955ef66827753b
Oktay Dinler <24858380+aoktayd@users.noreply.github.com>
2018-11-30 12:15:30 +0100

    template gemaakt met juiste layout nog geen data

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-md-center mt-5">
+    <div class="row justify-content-md-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Login</div>
