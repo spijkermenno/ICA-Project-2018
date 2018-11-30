210ee41c3afa38abe27183e7667435bc6cebb2c5
ivobreukers <ism.breukers@gmail.com>
2018-11-30 11:58:08 +0100

    Alfabet balk gemaakt, inclusief klik functie.

--- app/Http/Controllers/RubriekenController.php
+++ app/Http/Controllers/RubriekenController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\DatabaseCategoryRepository;
+use function Couchbase\defaultDecoder;
 
 class RubriekenController extends Controller
 {
@@ -30,8 +31,32 @@ class RubriekenController extends Controller
     public function index()
     {
         return view('rubrieken.rubrieken', [
+            'alphabet' => $this->getAlphabet(),
             'parents' => $this->categoryRepository->getAllParents(),
-            'children' => $this->categoryRepository->getAllChildren(),
+            'children' => $this->categoryRepository->getAllChildren()
         ]);
+    }
+
+    private function getAlphabet()
+    {
+        $alphabet = [];
+        for ($i = 65; $i < 91; $i++) {
+            $alphabet[] = [
+                'letter' => chr($i),
+                'active' => false
+            ];
+        }
+
+        $parents = $this->categoryRepository->getAllParents();
+
+        foreach ($alphabet as $key => $item) {
+            foreach ($parents as $parent) {
+                if ($item['letter'] == $parent->name[0] && $item['active'] == false) {
+                    $alphabet[$key]['active'] = true;
+                }
+            }
+        }
+
+        return $alphabet;
     }
 }
--- resources/views/rubrieken/rubrieken.blade.php
+++ resources/views/rubrieken/rubrieken.blade.php
@@ -5,37 +5,20 @@
         <h1>Rubrieken</h1>
         <div class="alphabet">
             <ul>
-                <li><a href="#A">A</a></li>
-                <li><a href="#B">B</a></li>
-                <li><a href="#C">C</a></li>
-                <li><a href="#D">D</a></li>
-                <li><a href="#E">E</a></li>
-                <li><a href="#F">F</a></li>
-                <li><a href="#G">G</a></li>
-                <li><a href="#H">H</a></li>
-                <li><a href="#I">I</a></li>
-                <li><a href="#J">J</a></li>
-                <li><a href="#K">K</a></li>
-                <li><a href="#L">L</a></li>
-                <li><a href="#M">M</a></li>
-                <li><a href="#N">N</a></li>
-                <li><a href="#O">O</a></li>
-                <li><a href="#P">P</a></li>
-                <li><a href="#Q">Q</a></li>
-                <li><a href="#R">R</a></li>
-                <li><a href="#S">S</a></li>
-                <li><a href="#T">T</a></li>
-                <li><a href="#U">U</a></li>
-                <li><a href="#V">V</a></li>
-                <li><a href="#W">W</a></li>
-                <li><a href="#X">X</a></li>
-                <li><a href="#Y">Y</a></li>
-                <li><a href="#Z">Z</a></li>
+                @foreach($alphabet as $item)
+                    <li>
+                        @if($item['active'] == true)
+                            <a href="#{{ $item['letter'] }}">{{ $item['letter'] }}</a>
+                        @else
+                            {{ $item['letter'] }}
+                        @endif
+                    </li>
+                @endforeach
             </ul>
         </div>
         <ul>
             @foreach($parents as $parent)
-                <li><a href="/rubrieken/{{ $parent->id }}">{{ $parent->name }}</a></li>
+                <li id="{{ $parent->name[0] }}"><a href="/rubrieken/{{ $parent->id }}">{{ $parent->name }}</a></li>
                 <ul>
                     @foreach($children as $child)
                         @if($child->parent == $parent->id)
