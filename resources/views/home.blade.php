ec47a7d3875f9a20cbe60b247b7ca0bb851d7275
Oktay Dinler <24858380+aoktayd@users.noreply.github.com>
2018-12-03 12:42:49 +0100

    homepagina en sass error gefixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        @include("breadcrumbs")
+        <div class="row">
+            <div class="col mb-3">
+                <h1>Populair</h1>
+            </div>
+        </div>
+        <div class="row mb-5">
             @for ($i = 0; $i < 3; $i++)
+                <div class="col">
+                    @component('product.card')
+                    @endcomponent
+                </div>
+            @endfor
+        </div>
+        <div class="row">
+            <div class="col mb-3">
+                <h1>Gauw voorbij!</h1>
+            </div>
+        </div>
+        @for ($i = 0; $i < 3; $i++)
+            <div class="row mb-5">
+                @for ($j = 0; $j < 4; $j++)
+                    <div class="col">
+                    @component('product.card')
+                    @endcomponent
+                    </div>
+                @endfor
+            </div>
+        @endfor
+    </div>
 @endsection
