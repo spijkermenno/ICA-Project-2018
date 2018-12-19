cf1703f3f4a40a88e304f4560f334cc21194384f
ivobreukers <ism.breukers@gmail.com>
2018-12-19 14:27:13 +0100

    Geen bieder afgevangen

--- resources/views/product/bids.blade.php
+++ resources/views/product/bids.blade.php
@@ -10,13 +10,24 @@
             <div class="card-header p-0 m-0 bg-primary text-white" id="headingOne">
                 <div class="row pt-2 pl-2 position-relative" data-toggle="collapse" data-target="#biedinglijst">
                     <div class="col-8 col-md-8 col-lg-8"
-                        style="font-size:20px;">{{ $bids[0]->user_name }}</div>
-                    <div class="col-4 col-md-4 col-lg-4 badge badge-secondary"
-                        style="min-width: 40px; max-width: 60px; line-height: 15px; max-height: 22px; margin-top: 8px;">€{{$bids[0]->highest_bid}}</div>
+                        style="font-size:20px;">
+                        @if($bids[0]->date == "")
+                            Geen biedingen
+                        @else
+                            {{ $bids[0]->user_name }}
+                        @endif
+                    </div>
+                    @if($product->start_price != $bids[0]->highest_bid)
+                        <div class="col-4 col-md-4 col-lg-4 badge badge-secondary"
+                            style="min-width: 40px; max-width: 60px; line-height: 15px; max-height: 22px; margin-top: 8px;">
+                                €{{$bids[0]->highest_bid}}
+                        </div>
+                    @endif
                 </div>
                 <div class="row pr-1 pt-0 pl-2">
                     <h6 class="col-12 col-md-12 col-lg-12">
-                        @if($bids[0]->date != "")
+                        @if($bids[0]->date == "")
+                        @else
                             {{ \Illuminate\Support\Carbon::parse($bids[0]->date)->diffForHumans() }}
                         @endif
                     </h6>
