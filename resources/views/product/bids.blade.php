7164928b6f366332c3b08ddd5e8e70eb4f5a6a7e
ivobreukers <ism.breukers@gmail.com>
2018-12-19 13:51:10 +0100

    Updated style

--- resources/views/product/bids.blade.php
+++ resources/views/product/bids.blade.php
@@ -9,9 +9,10 @@
         <div class="card">
             <div class="card-header p-0 m-0 bg-primary text-white" id="headingOne">
                 <div class="row pt-2 pl-2 position-relative" data-toggle="collapse" data-target="#biedinglijst">
-                    <h5 class="col-9 col-md-8 col-lg-8">{{$bids[0]->user_name}}</h5>
-                    <h5 class="col-3 col-md-4 col-lg-4 badge badge-secondary"
-                        style="min-width: 40px; max-width: 60px; line-height: 15px">€{{$bids[0]->highest_bid}}</h5>
+                    <div class="col-8 col-md-8 col-lg-8"
+                        style="font-size:20px;">{{ $bids[0]->user_name }}</div>
+                    <div class="col-4 col-md-4 col-lg-4 badge badge-secondary"
+                        style="min-width: 40px; max-width: 60px; line-height: 15px; max-height: 22px; margin-top: 8px;">€{{$bids[0]->highest_bid}}</div>
                 </div>
                 <div class="row pr-1 pt-0 pl-2">
                     <h6 class="col-12 col-md-12 col-lg-12">{{ \Illuminate\Support\Carbon::parse($bids[0]->date)->diffForHumans() }}</h6>
@@ -27,7 +28,7 @@
                                 <div class="col-4 col-md-4 col-lg-4 badge badge-secondary"
                                     style="min-width: 40px; max-width: 60px; line-height: 15px">€{{$bid->highest_bid}}</div>
                             </div>
-                            <div class="row p-1 pl-2 border-bottom">
+                            <div class="row pr-1 pt-0 pl-2 border-bottom">
                                 <h6 class="col-12 col-md-12 col-lg-12">{{ \Illuminate\Support\Carbon::parse($bid->date)->diffForHumans() }}</h6>
                             </div>
                         @endif
