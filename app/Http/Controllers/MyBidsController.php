64b8e49dbe7b04883dfb03f94d091574710637d1
Tom Esendam <tom.esendam@gmail.com>
2019-01-08 14:48:54 +0100

    cs fix

--- app/Http/Controllers/MyBidsController.php
+++ app/Http/Controllers/MyBidsController.php
@@ -11,16 +11,17 @@ class MyBidsController extends Controller
     * @var DatabaseBidsRepository
     */
     private $bidsRepository;
+    private $itemRepository;
 
     /**
      * MyBidsController constructor.
      * @param DatabaseCategoryRepository $categoryRepository
      * @param DatabaseBidsRepository $bidsRepository
      */
-    public function __construct(DatabaseCategoryRepository $categoryRepository, DatabaseBidsRepository $bidsRepository)
+    public function __construct(DatabaseCategoryRepository $categoryRepository, DatabaseBidsRepository $bidsRepository, \App\Repositories\DatabaseItemRepository $itemRepository)
     {
         parent::__construct($categoryRepository);
-
+        $this->itemRepository = $itemRepository;
         $this->bidsRepository = $bidsRepository;
     }
 
@@ -30,7 +31,13 @@ class MyBidsController extends Controller
      */
     public function __invoke(string $user)
     {
+        $lel;
         /*@TODO NAME IS SUBJECT TO CHANGE*/
+        foreach ($this->bidsRepository->getAllByUser($user) as $ff) {
+            if (!isset($ff)) {
+                return view('my_bids_view', ['result' => false]);
+            }
+        }
         return view('my_bids_view', ['bids' => $this->bidsRepository->getAllByUser($user)]);
     }
 }
